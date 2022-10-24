286b82a45ccbc537722bd28b217f736dd5b7bd72
petersjang1993 <petersjang1993@gmail.com>
2022-10-24 19:51:08 +0000

    Completed authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,51 @@
 class UsersController < ApplicationController
+  def authenticate
+    # get the username from params
+    # get the password from params
+
+    # look up the record from the db matching username
+    # if there's no record, redirect back to sign in form
+
+    # if there's a record, check to see if password matches
+    #if not, redirect back to sign in form
+
+    # if so, set the cookie 
+    # redirect to homepage
+
+    un = params.fetch("input_username")
+    pw = params.fetch("input_password")
+
+    user = User.where({ :username => un }).at(0)
+
+    if user == nil
+      redirect_to("/user_sign_in", { :alert => "No one by that name round these parts" })
+    else
+      if user.authenticate(pw)
+        session.store(:user_id, user.id)
+
+        redirect_to("/", { :notice => "Welcome back, " + user.username + "!" })
+      else 
+        redirect_to("/user_sign_in", { :alert => "Nice try, sucker! " })
+      end
+    end
+  end 
+
+  def toast_cookies
+    reset_session
+
+    redirect_to("/", { :notice => "See ya later!" })
+  end
+
+  def new_registration_form
+
+    render({ :template => "users/signup_form.html.erb" })
+  end
+
+  def new_session_form 
+    
+    render({ :template => "users/signin_form.html.erb" })
+  end
+
   def index
     @users = User.all.order({ :username => :asc })
 
@@ -16,10 +63,18 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
 
-    user.save
+    save_status = user.save
 
-    redirect_to("/users/#{user.username}")
+    if save_status == true
+      session.store(:user_id, user.id)
+
+      redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + "!" })
+    else
+      redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence })
+    end
   end
 
   def update
@@ -31,7 +86,7 @@ class UsersController < ApplicationController
 
     user.save
     
-    redirect_to("/users/#{user.username}")
+    redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + "!" })
   end
 
   def destroy
